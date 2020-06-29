5fdd6137c1619f857321020dcf5cba4ed7c9dafa
Michele Curtaz <mcurtaz@gmail.com>
2020-06-29 14:51:28 +0200

    Creata Funzione Palindromi

--- palindromi/script.js
+++ palindromi/script.js
@@ -2,15 +2,33 @@
 
 // creo un ciclo for con un numero di cicli uguale alla lunghezza della parola in modo da verificare tutte le lettere. confronto ogni lettera con quella in posizione speculare. (prima con ultima, seconda con penultima...) se tutte sono uguali allora la parola è palindroma altrimenti no.
 
-var parola = prompt("Inserisci una parola e ti dirò se è palindroma:");
+var word = prompt("Inserisci una parola e ti dirò se è palindroma:");
 
-// creo una variabile d'appoggio
-var palindroma = true;
-for (var i = 0; i < parola.length; i++) {
-  if (parola[i] != parola[parola.length - 1 - i]) {
-    palindroma = false;
-  }
- console.log(parola[i], parola[parola.length -1 - i]);
+if (isPalindroma(word)){
+  console.log("è palindroma");
+}else{
+  console.log("Non è palindroma");
 }
 
-console.log(palindroma);
+
+
+
+
+
+//functions
+
+
+function isPalindroma(parola) {
+
+  var palindroma = true;
+
+  for (var i = 0; i < parola.length; i++) {
+    if (parola[i] != parola[parola.length - 1 - i]) {
+      palindroma = false;
+    }
+
+  }
+
+  return palindroma;
+
+}
